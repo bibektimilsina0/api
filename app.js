dc1d62650d098f1d9ee9f5bad23537c52434bcf7
bikkibro <bibektimilsina7857@gmail.com>
2023-06-28 22:25:26 +0545

    nn

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express=require('express')
 const app=express();
+const {people}=require('./data')
 const {readFile} = require('fs')
-
+app.use(express.static('./methods-public'))
+app.get('/api/people',(req,res)=>{
+   res.status(200).json(people)
+})
 app.get('/api/crypto',(req,res)=>{
    const file =readFile('./cryptoapi/place.json','utf8',(err,data)=>{
       res.status(200).send(data)
